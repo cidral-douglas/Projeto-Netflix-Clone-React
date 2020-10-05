c0277b187ca7595d4868940559c3e4707994b40f
Douglas Cidral <doug.coding20@gmail.com>
2020-10-05 15:24:41 -0300

    MovieRow: First Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow';
 
 function App() {
+
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(()=>{
+    const loadAll = async () => {
+      // Pegando a lista TOTAL
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+    }
+
+    loadAll();
+  },[]);
+
   return (
-    <div>
-      Netflix Clone
+    <div className="page">
+
+      <section className="lists">
+        {movieList.map((item, key)=>(
+          <MovieRow key={key} title={item.title} items={item.items}></MovieRow>
+        ))}
+      </section>
+
     </div>
   );
 }
