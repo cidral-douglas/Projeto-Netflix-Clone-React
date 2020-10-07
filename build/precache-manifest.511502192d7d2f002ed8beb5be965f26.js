766d6a4c7961b3e44076fa31577530c2e490a0d1
Douglas Cidral <doug.coding20@gmail.com>
2020-10-07 10:55:50 -0300

    Build

---
+++ build/precache-manifest.511502192d7d2f002ed8beb5be965f26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aea2473b987c5574eabd25bbe2dac60",
+    "url": "./index.html"
+  },
+  {
+    "revision": "71bb145c58b070e67479",
+    "url": "./static/css/main.53cd901e.chunk.css"
+  },
+  {
+    "revision": "473df69fad01febb2dae",
+    "url": "./static/js/2.29beb290.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.29beb290.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71bb145c58b070e67479",
+    "url": "./static/js/main.e94568d2.chunk.js"
+  },
+  {
+    "revision": "d275fbb4dd87b3a4b843",
+    "url": "./static/js/runtime-main.c1700fe6.js"
+  }
+]);
